60f098e9f80a146a95924d96677a498fdfac7abd
Fernando Agüero <fjaguero@gmail.com>
2016-07-10 21:57:02 +0200

    Add developer tools

--- react-client-app/app/redux/store.js
+++ react-client-app/app/redux/store.js
@@ -1,7 +1,9 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import mainReducer from './reducers';
 import thunk from 'redux-thunk';
 
-const store = createStore(mainReducer, applyMiddleware(thunk));
+const store = createStore(mainReducer, compose(
+  applyMiddleware(thunk
+), window.devToolsExtension ? window.devToolsExtension() : f => f));
 
 export default store;
